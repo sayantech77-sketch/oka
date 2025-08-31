0a286adaca41b65d0037b5515eed98c27260d349
sayantech77-sketch <sayantech77@gmail.com>
2025-08-31 21:40:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,22 @@
  */
 
 import { AppRegistry } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React from 'react';
 import App from './App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// If you use Redux or any global providers, import them here
+// import { Provider } from 'react-redux';
+// import store from './src/store';
+
+const Main = () => (
+  <SafeAreaProvider>
+    {/* Wrap with Redux Provider if using Redux */}
+    {/* <Provider store={store}> */}
+      <App />
+    {/* </Provider> */}
+  </SafeAreaProvider>
+);
+
+AppRegistry.registerComponent(appName, () => Main);
